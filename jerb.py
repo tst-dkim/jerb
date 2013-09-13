1d39119968e3aa167539aa3d194b4fc827c2af31
Devon Kim <dkim@t-sciences.com>
2013-09-13 13:24:02 -0400

    Print hostname in the output too

--- jerb.py
+++ jerb.py
@@ -1,7 +1,10 @@
 import os
+import socket
 
 # do something useful that makes me money
 print os.uname()
+# ip address? hopefully!
+print socket.gethostbyname(socket.gethostname())
 
 # uncomment to do nothing useful except return a non-zero error code
 #sys.exit(1)
